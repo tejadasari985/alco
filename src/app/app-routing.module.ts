44995fb167f0b52ebc35c0baf41da1ed7c21e1d8
tejadasari985 <tejadasari985@gmail.com>
2022-01-03 23:09:00 +0530

    message

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'footer',component:FooterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
